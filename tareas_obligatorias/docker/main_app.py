9b7c38a800ab324d444ea5d029684e9bb181bf23
Julen <jfuentes019@ikasle.ehu.eus>
2022-12-06 14:28:53 +0100

    first commit

---
+++ tareas_obligatorias/docker/main_app.py
@@ -0,0 +1,35 @@
+#### Importar librerías necesarias ####
+from rethinkdb import r              
+import subprocess
+
+
+#### Programa principal ####
+if __name__ == "__main__":
+
+    # Declarar variables necesarias para la conexión con la base de datos 
+    host = 'servidor-redb'
+    port = 28015
+
+    # Establecer conexión con la base de datos
+    r.connect(host = host, port = port).repl()
+    subprocess.run(["echo", "Se ha establecido la conexión con la base de datos"])
+  
+    # Crear una nueva tabla
+    try:
+        r.table_create('entrega_individual').run()
+        subprocess.run(["echo", "Se ha creado la tabla 'entrega_individual' con éxito"])
+    except:
+        subprocess.run(["echo", "No se ha creado ninguna tabla. 'entrega_individual' ya existe"])
+
+    # Insertar datos en la tabla recién creada
+    try:
+        r.table('entrega_individual').insert({
+            'id': '1',
+            'tarea_1': 'Explicar funcionalidad', 
+            'tarea_2': 'Programar cliente', 
+            'tarea_3': 'Subir imagen a DockerHub', 
+            'tarea_4': 'Crear entorno compose'
+        }).run()
+        subprocess.run(["echo", "Se ha subido un dato a la tabla 'entrega_individual'"]) 
+    except:
+        subprocess.run(["echo", "No se ha subido ningún dato. El dato ya está en la tabla"])
